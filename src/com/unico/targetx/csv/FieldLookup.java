4a834b81491861949502d173dcbbd787213fa796
Baba Unico <baba.kanagaraju@unicosolution.com>
2017-04-14 19:24:34 +0530

    File name changed.

--- src/com/unico/targetx/csv/FieldLookup.java
+++ src/com/unico/targetx/csv/FieldLookup.java
@@ -26,7 +26,7 @@ public class FieldLookup {
 	
 	private FieldLookup() throws IOException {	
 		
-		csvdefFile = "resource/csvdef1.csv";
+		csvdefFile = "resource/csvdef.csv";
 		File file = new File(csvdefFile);
 		
 		if(file.exists()) {
