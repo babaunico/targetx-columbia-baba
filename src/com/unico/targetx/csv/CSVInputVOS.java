2ea142c9d8a32d1be9eb5e5a19b735dce11f36fe
Baba Unico <baba.kanagaraju@unicosolution.com>
2017-04-17 20:03:56 +0530

    A new VO class is added

---
+++ src/com/unico/targetx/csv/CSVInputVOS.java
@@ -0,0 +1,56 @@
+package com.unico.targetx.csv;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.supercsv.io.CsvMapReader;
+import org.supercsv.io.ICsvMapReader;
+import org.supercsv.prefs.CsvPreference;
+
+public class CSVInputVOS {
+
+	private File file = null;
+	
+	CsvPreference prefs = CsvPreference.STANDARD_PREFERENCE;
+	private ICsvMapReader mapReader = null;
+	private Map<String, String> row;
+	private List<Map<String, String>> parsedRows;
+	
+	public CSVInputVOS(String filePath) throws IOException { 
+		
+		file = new File(filePath);
+		if(file.exists()) {
+			mapReader = new CsvMapReader(new FileReader(file), prefs);
+			final String[] readHeader = mapReader.getHeader(true);
+			final String[] writeHeader = new String[readHeader.length + 1];
+			System.arraycopy(readHeader, 0, writeHeader, 0, readHeader.length);		
+			readAndWriteRow(mapReader, readHeader);
+		} else {
+			System.out.println(filePath + "File Not Found");
+		}
+	}
+	
+	public void readAndWriteRow(ICsvMapReader mapReader, final String[] readHeader) throws IOException {	
+		
+		parsedRows = new ArrayList<Map<String, String>>();
+		while( (row = mapReader.read(readHeader)) != null ) {
+			parsedRows.add(row);
+		}
+	} 
+	
+	public Map<String, String> getRowForGivenId(int rowId) {
+		
+		Map<String, String> row = null;
+		try {
+			row = parsedRows.get(rowId);
+		} catch (IndexOutOfBoundsException ae) {
+			System.out.println("Given element does not exist..");
+		}
+		
+		return row;
+	}
+}
