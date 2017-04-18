fd7258ba6c5dce1d0b4c420297b3520f280230e1
Baba Unico <baba.kanagaraju@unicosolution.com>
2017-04-18 16:19:40 +0530

    getRowForGivenID method is added

--- src/com/unico/targetx/csv/CSVVOClient.java
+++ src/com/unico/targetx/csv/CSVVOClient.java
@@ -1,16 +1,36 @@
 package com.unico.targetx.csv;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Map;
 
 public class CSVVOClient {
 
+	private static Collection<String> rowValues;
+	private static Map<String, String> row;
+
 	public static void main(String[] args) throws IOException {
 		String csvInputFilePath = "resource/inputcsv.csv";
-		
 		CSVInputVOS csvInputVOS = new CSVInputVOS(csvInputFilePath);
 		
-		Map<String, String> row = csvInputVOS.getRowForGivenId(7);
+		getRowForGivenIndexValue(csvInputVOS);
+		
+		getRowForGivenIDValue(csvInputVOS);
+	}
+
+	private static void getRowForGivenIDValue(CSVInputVOS csvInputVOS) {
+		rowValues = csvInputVOS.getRowForGivenID("2");
+		if(rowValues != null) {
+			for (String rowValue : rowValues) {
+				System.out.print(rowValue);
+			}
+		} else {
+			System.out.println("Given ID does not exist");
+		}
+	}
+
+	private static void getRowForGivenIndexValue(CSVInputVOS csvInputVOS) {
+		row = csvInputVOS.getRowForGivenIndex(0);
 		if(row != null) {
 			for (Map.Entry<String, String> entry : row.entrySet()) {
 				System.out.print(entry.getValue());
